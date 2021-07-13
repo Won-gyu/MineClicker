01dc39832dc3c3f60032926d75f94817bca43458
wongyuh <wongyuh36@gmail.com>
2021-07-14 00:16:44 +0900

    Updated

--- Assets/Scripts/App/GameObject/Miner.cs
+++ Assets/Scripts/App/GameObject/Miner.cs
@@ -4,59 +4,79 @@ using UnityEngine;
 
 namespace Mine
 {
-    public enum MinerState
+    public enum MinerActionState
     {
         FindMineral,
         Dig,
         Deliver
     }
 
+    public enum MinerBodyState
+    {
+        Stand,
+        Walk,
+        Hit,
+        Carry
+    }
+
     public class Miner : MovableObject
     {
+        public const string ANI_PARAM_STATE = "State";
+
+        [SerializeField]
+        private Animator animator;
         public Floor currentFloor;
         public Basement basementWorkPlace;
         public ElevatorArea elevator;
         public Mineral mineral;
-        public MinerState state;
+        public MinerActionState actionState;
+        public MinerBodyState bodyState;
         public float delayDig;
-        public Sprite sprite;
 
         [SerializeField]
         private PathController pathController;
 
         public GoalOnFloor goal;
         public Vector2 positionGoal;
-        private Coroutine coroutineState;
+        private Coroutine coroutineActionState;
+        private bool carry;
 
         private void Start()
         {
-            ChangeState(MinerState.FindMineral);
+            ChangeActionState(MinerActionState.FindMineral);
         }
 
         public void Init(Floor floorWorkPlace)
         {
             this.currentFloor = floorWorkPlace;
             this.basementWorkPlace = floorWorkPlace as Basement;
+            ChangeBodyState(MinerBodyState.Stand);
         }
 
-        private void ChangeState(MinerState state)
+        public void ChangeBodyState(MinerBodyState bodyState)
         {
-            this.state = state;
-            if (coroutineState != null)
+            this.bodyState = bodyState;
+            animator.SetInteger(ANI_PARAM_STATE, (int)bodyState);
+        }
+
+        private void ChangeActionState(MinerActionState actionState)
+        {
+            this.actionState = actionState;
+            if (coroutineActionState != null)
             {
-                StopCoroutine(coroutineState);
-                coroutineState = null;
+                StopCoroutine(coroutineActionState);
+                coroutineActionState = null;
             }
-            switch (state)
+            switch (actionState)
             {
-                case MinerState.FindMineral:
-                    coroutineState = StartCoroutine(StateCoroutineFindingMineral());
+                case MinerActionState.FindMineral:
+                    coroutineActionState = StartCoroutine(StateCoroutineFindingMineral());
                     break;
-                case MinerState.Dig:
-                    coroutineState = StartCoroutine(StateCoroutineDig());
+                case MinerActionState.Dig:
+                    coroutineActionState = StartCoroutine(StateCoroutineDig());
                     break;
-                case MinerState.Deliver:
-                    coroutineState = StartCoroutine(StateCoroutineDeliver());
+                case MinerActionState.Deliver:
+                    coroutineActionState = StartCoroutine(StateCoroutineDeliver());
                     break;
             }
         }
@@ -66,30 +86,46 @@ namespace Mine
         {
             mineral = basementWorkPlace.GetRandomMineral();
             yield return StartCoroutine(CoroutineWalkTo(mineral.GoalOnFloor));
-            ChangeState(MinerState.Dig);
+            ChangeActionState(MinerActionState.Dig);
         }
 
         private IEnumerator StateCoroutineDig()
         {
+            ChangeBodyState(MinerBodyState.Hit);
             yield return new WaitForSecondsRealtime(delayDig);
-            ChangeState(MinerState.Deliver);
+            ChangeActionState(MinerActionState.Deliver);
         }
 
         private IEnumerator StateCoroutineDeliver()
         {
+            StartCarry();
             yield return StartCoroutine(CoroutineWalkTo(basementWorkPlace.GoalElevator));
             yield return StartCoroutine(CoroutineMoveToFloor(SpaceManager.Instance.Surface.FloorLevel));
             yield return StartCoroutine(CoroutineWalkTo(SpaceManager.Instance.Surface.DropOff));
+            EndCarry();
+
+            ChangeBodyState(MinerBodyState.Stand);
             yield return StartCoroutine(CoroutineWalkTo(SpaceManager.Instance.Surface.GoalElevator));
             yield return StartCoroutine(CoroutineMoveToFloor(basementWorkPlace.FloorLevel));
 
-            ChangeState(MinerState.FindMineral);
+            ChangeActionState(MinerActionState.FindMineral);
+        }
+
+        private void StartCarry()
+        {
+            carry = true;
+        }
+
+        private void EndCarry()
+        {
+            carry = false;
         }
 
 
         // Action
         private IEnumerator CoroutineWalkTo(GoalOnFloor goal)
         {
+            ChangeBodyState(carry ? MinerBodyState.Carry : MinerBodyState.Walk);
             this.goal = goal;
             positionGoal = goal.GetRandomPositionGoal();
             Direction = GetDirectionX(positionGoal);
@@ -98,10 +134,12 @@ namespace Mine
                 Position += Speed;
                 yield return null;
             }
+            ChangeBodyState(MinerBodyState.Stand);
         }
 
         private IEnumerator CoroutineWaitForElevator(int spaceIdElevator, int floorLevelWaiting, int floorLevelGoal)
         {
+            ChangeBodyState(carry ? MinerBodyState.Carry : MinerBodyState.Stand);
             elevator = SpaceManager.Instance.GetSpace(spaceIdElevator).GetComponent<ElevatorArea>();
             elevator.AddMinerWaiting(this, floorLevelWaiting, floorLevelGoal);
             while (elevator != null)

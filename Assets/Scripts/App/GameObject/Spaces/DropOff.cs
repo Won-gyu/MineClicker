9f4c5d957a409491392ace0af984eabfceae718c
wongyuh <wongyuh36@gmail.com>
2021-07-23 02:11:30 +0900

    Updated

--- Assets/Scripts/App/GameObject/Spaces/DropOff.cs
+++ Assets/Scripts/App/GameObject/Spaces/DropOff.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 namespace Mine
 {
+    public enum OrePileSize
+    {
+        Small,
+        Mid,
+        Big,
+        VeryBig
+    }
+
+    public class PileSet
+    {
+        public PileSet()
+        {
+            orePileCounts = new List<int>();
+            orePileIds = new List<int>();
+        }
+        public List<int> orePileCounts;
+        public List<int> orePileIds;
+    }
+
     public class DropOff : MonoBehaviour
     {
-        [SerializeField]
-        private List<SpriteRenderer> renderers; 
+        private List<GameObject> orePiles;
+        private PileSet pileSet;
+
     }
 }
